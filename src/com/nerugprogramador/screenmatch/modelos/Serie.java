3f0083f1592b1c0534582aa303807aa3f9b85e8a
Nerug-Programador <williamgermanarangurenbuirago@gmail.com>
2024-10-26 18:05:26 -0500

    Actualización: Aprovechando el modelo para series.

---
+++ src/com/nerugprogramador/screenmatch/modelos/Serie.java
@@ -0,0 +1,31 @@
+package com.nerugprogramador.screenmatch.modelos;
+
+public class Serie extends Titulo{
+    private int temporadas;
+    private int episodiosPorTemporada;
+    private int minutosPorEpisodio;
+
+    public int getTemporadas() {
+        return temporadas;
+    }
+
+    public void setTemporadas(int temporadas) {
+        this.temporadas = temporadas;
+    }
+
+    public int getMinutosPorEpisodios() {
+        return minutosPorEpisodio;
+    }
+
+    public void setMinutosPorEpisodios(int minutosPorEpisodios) {
+        this.minutosPorEpisodio = minutosPorEpisodios;
+    }
+
+    public int getEpisodiosPorTemporada() {
+        return episodiosPorTemporada;
+    }
+
+    public void setEpisodiosPorTemporada(int episodiosPorTemporada) {
+        this.episodiosPorTemporada = episodiosPorTemporada;
+    }
+}
