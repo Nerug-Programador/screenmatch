24527be4683fe1a804b60774ae302824c1b44980
Nerug-Programador <williamgermanarangurenbuirago@gmail.com>
2024-11-09 15:18:13 -0500

    Actualización:  Deserializando los datos.

---
+++ src/main/java/com/nerugdev/screenmatch/model/DatosSerie.java
@@ -0,0 +1,9 @@
+package com.nerugdev.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+
+public record DatosSerie(
+        @JsonAlias ("Title") String titulo,
+        @JsonAlias ("totalSeasons")Integer totalDeTemporadas,
+        @JsonAlias ("imdbRating") String evaluacion) {
+}
