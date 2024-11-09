61a74a37d0ce8128163c742c47be0fbc771550e7
Nerug-Programador <williamgermanarangurenbuirago@gmail.com>
2024-11-09 14:58:45 -0500

    Actualización:  Consumiendo datos de las series.

--- src/main/java/com/nerugdev/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/nerugdev/screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,6 @@
 package com.nerugdev.screenmatch;
 
+import com.nerugdev.screenmatch.service.ConsumoAPI;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +14,9 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hola mundo! desde Spring");
+		var consumoApi = new ConsumoAPI();
+//		var json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=matrix&apikey=c7700e77");
+		var json = consumoApi.obtenerDatos("https://coffee.alexflipnote.dev/random.json");
+		System.out.println(json);
 	}
 }
