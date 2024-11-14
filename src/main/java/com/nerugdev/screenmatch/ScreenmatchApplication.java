42e4c0b6df80ffd4cc18eb8dbc553dafe6ef0fcf
Nerug-Programador <williamgermanarangurenbuirago@gmail.com>
2024-11-14 11:25:36 -0500

    Actualización:  Modelando las temporadas

--- src/main/java/com/nerugdev/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/nerugdev/screenmatch/ScreenmatchApplication.java
@@ -2,11 +2,15 @@ package com.nerugdev.screenmatch;
 
 import com.nerugdev.screenmatch.model.DatosEpisodio;
 import com.nerugdev.screenmatch.model.DatosSerie;
+import com.nerugdev.screenmatch.model.DatosTemporadas;
 import com.nerugdev.screenmatch.service.ConsumoAPI;
 import com.nerugdev.screenmatch.service.ConvirerteDatos;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -27,5 +31,13 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=game%20of%20thrones&Season=1&episode=1&apikey=c7700e77");
 		DatosEpisodio episodios = conversor.obtenerDatos(json, DatosEpisodio.class);
 		System.out.println(episodios);
+
+		List<DatosTemporadas> temporadas = new ArrayList<>();
+		for (int i = 1; i <= datos.totalDeTemporadas() ; i++) {
+			json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=game%20of%20thrones&Season="+i+"&apikey=c7700e77");
+			var datosTemporadas = conversor.obtenerDatos(json, DatosTemporadas.class);
+			temporadas.add(datosTemporadas);
+		}
+		temporadas.forEach(System.out::println);
 	}
 }
