dd6ea1cb25fe6059dcc411d2579e629659800775
Nerug-Programador <williamgermanarangurenbuirago@gmail.com>
2024-11-20 11:51:57 -0500

    Actualización:  Creando un mapa de datos por temporada.

--- src/main/java/com/nerugdev/screenmatch/principal/Principal.java
+++ src/main/java/com/nerugdev/screenmatch/principal/Principal.java
@@ -89,7 +89,7 @@ public class Principal {
                 ));*/
 
         // Busca episodio por un pedazo de titulo
-        System.out.println("Por favor escriba el titulo del episodio que desea ver");
+/*        System.out.println("Por favor escriba el titulo del episodio que desea ver");
         var pedazoTituo  = teclado.nextLine();
         Optional<Episodio> episodioBuscado = episodios.stream()
                 .filter(e -> e.getTitulo().toUpperCase().contains(pedazoTituo.toUpperCase()))
@@ -99,6 +99,11 @@ public class Principal {
             System.out.println("Los datos son: " + episodioBuscado.get());
         }else{
             System.out.println("Episodio no encontrado");
-        }
+        }*/
+        Map<Integer, Double> evaluacionesPorTemporada = episodios.stream()
+                .filter(e -> e.getEvaluacion() > 0.0)
+                .collect(Collectors.groupingBy(Episodio::getTemporada,
+                        Collectors.averagingDouble(Episodio::getEvaluacion)));
+        System.out.println(evaluacionesPorTemporada);
     }
 }
